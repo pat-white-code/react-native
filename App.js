d5100e5cee670f0011e78819324466d2340f67e3
Patrick White <pat.k.white@gmail.com>
2024-04-08 16:23:13 -0500

    feat: connect to graphql server

--- App.js
+++ App.js
@@ -2,19 +2,32 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 // import CurrentWeather from "./src/components/CurrentWeather";
 import UpcomingWeather from "./src/components/UpcomingWeather";
+import { AppRegistry } from "react-native";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { gql } from "@apollo/client";
+import config from "./config";
+
+const client = new ApolloClient({
+    uri: `http://${config.IP_ADDRESS}:4000/graphql`,
+    cache: new InMemoryCache(),
+});
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
 });
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <UpcomingWeather />
-    </View>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <View style={styles.container}>
+                <UpcomingWeather />
+            </View>
+        </ApolloProvider>
+    );
 };
+
+AppRegistry.registerComponent("MyApplication", () => App);
 
 export default App;
--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,70 +1,90 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, FlatList, Text, View, StatusBar, ImageBackground } from "react-native";
-import { Feather } from "@expo/vector-icons";
 
-const data = [
-    {
-        weather: {
-            main: "Rain",
-            description: "moderate rain",
-        },
-        main: {
-            feels_like: 298.74,
-            temp_min: 297.56,
-            temp_max: 300.05,
-        },
-        dt: 1661870592,
-    },
-    {
-        weather: {
-            main: "Rain",
-            description: "moderate rain",
-        },
-        main: {
-            feels_like: 243.74,
-            temp_min: 544.56,
-            temp_max: 123.05,
-        },
-        dt: 1662070592,
-    },
-    {
-        weather: {
-            main: "Rain",
-            description: "moderate rain",
-        },
-        main: {
-            feels_like: 345.74,
-            temp_min: 123.56,
-            temp_max: 534.05,
-        },
-        dt: 1662270592,
-    },
-];
+import {
+    StyleSheet,
+    SafeAreaView,
+    FlatList,
+    Text,
+    View,
+    StatusBar,
+    ImageBackground,
+} from "react-native";
+// import { Feather } from "@expo/vector-icons";
 
-const EmptyWeather = () =>( 
+import { gql, useQuery } from "@apollo/client";
+//
+
+const GET_POSTS = gql`
+    query Query {
+        posts {
+            id
+            body
+        }
+    }
+`;
+
+// const data = [
+//     {
+//         weather: {
+//             main: "Rain",
+//             description: "moderate rain",
+//         },
+//         main: {
+//             feels_like: 298.74,
+//             temp_min: 297.56,
+//             temp_max: 300.05,
+//         },
+//         dt: 1661870592,
+//     },
+//     {
+//         weather: {
+//             main: "Rain",
+//             description: "moderate rain",
+//         },
+//         main: {
+//             feels_like: 243.74,
+//             temp_min: 544.56,
+//             temp_max: 123.05,
+//         },
+//         dt: 1662070592,
+//     },
+//     {
+//         weather: {
+//             main: "Rain",
+//             description: "moderate rain",
+//         },
+//         main: {
+//             feels_like: 345.74,
+//             temp_min: 123.56,
+//             temp_max: 534.05,
+//         },
+//         dt: 1662270592,
+//     },
+// ];
+
+const EmptyWeather = () => (
     <View>
         <Text>No Weather Data Available</Text>
     </View>
-)
-
+);
 
 const container = {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
-    backgroundColor: 'royalblue'
+    backgroundColor: "royalblue",
 };
 
 const item = {
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
     borderWidth: 5,
-    borderColor: 'black',
-    backgroundColor: 'pink'
-}
+    borderColor: "black",
+    backgroundColor: "pink",
+};
 
 const styles = StyleSheet.create({
     container,
@@ -73,48 +93,80 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     date: {
-        color: 'white',
+        color: "white",
         fontSize: 15,
     },
     temp: {
-        color: 'white',
+        color: "white",
         fontSize: 18,
-    }
+    },
 });
 
-const WeatherItem = (weather) => {
-    const { dt, min, max, condition } = weather;
-    return (
-        <View style={styles.item}>
-            <Feather name={"sun"} size={50} color={"white"} />
-            <Text style={styles.date}>{dt}</Text>
-            <Text style={styles.temp}>{min}</Text>
-            <Text style={styles.temp}>{max}</Text>
-        </View>
-    );
-};
+// const WeatherItem = (weather) => {
+//     const { dt, min, max } = weather;
+//     // console.log("response", response);
+//     // console.log(response);
+//     return (
+//         <View style={styles.item}>
+//             <Feather name={"sun"} size={50} color={"white"} />
+//             <Text style={styles.date}>{dt}</Text>
+//             <Text style={styles.temp}>{min}</Text>
+//             <Text style={styles.temp}>{max}</Text>
+//         </View>
+//     );
+// };
 
 const UpcomingWeather = () => {
     // const { container, item } = styles;
-    const renderWeatherItem = ({ item }) => (
-        <WeatherItem
-            condition={item.weather.main}
-            dt={item.dt}
-            min={item.main.temp_min}
-            max={item.main.temp_max}
-        ></WeatherItem>
-    );
+    const response = useQuery(GET_POSTS);
+    // console.log("data", response);
+    // console.log(response.error)
+    const { loading, error, data } = response;
+    // const renderWeatherItem = ({ item }) => (
+    //     <WeatherItem
+    //         condition={item.weather.main}
+    //         dt={item.dt}
+    //         min={item.main.temp_min}
+    //         max={item.main.temp_max}
+    //     ></WeatherItem>
+    // );
+    if (loading) {
+        return (
+            <View>
+                <Text>Loading</Text>
+            </View>
+        );
+    }
     return (
         <SafeAreaView style={styles.container}>
-            <ImageBackground style={styles.image} source={require('../../assets/lightning.jpg')}>
-            <Text>Upcoming Weather</Text>
-            <FlatList
-                data={data}
-                renderItem={renderWeatherItem}
-                keyExtractor={(item) => item.dt}
-                // ItemSeparatorComponent={() => <View style={{ backgroundColor: 'red', height: 2 }} />}
-                ListEmptyComponent={<EmptyWeather />}
-            />
+            <ImageBackground
+                style={styles.image}
+                source={require("../../assets/lightning.jpg")}
+            >
+                <Text>Upcoming Weather</Text>
+                {loading && (
+                    <View>
+                        <Text>Loading posts</Text>
+                    </View>
+                )}
+                {data && (
+                    <FlatList
+                        data={data.posts}
+                        renderItem={(item) => (
+                            <View>
+                                <Text>{item.body}</Text>
+                            </View>
+                        )}
+                        keyExtractor={(item) => item.id}
+                    />
+                )}
+                {/* <FlatList
+                    data={data}
+                    renderItem={renderWeatherItem}
+                    keyExtractor={(item) => item.dt}
+                    // ItemSeparatorComponent={() => <View style={{ backgroundColor: 'red', height: 2 }} />}
+                    ListEmptyComponent={<EmptyWeather />}
+                /> */}
             </ImageBackground>
         </SafeAreaView>
     );
